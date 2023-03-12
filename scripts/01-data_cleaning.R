0709da43e9b5e650786d9a12b0036d2ae9b6cc28
Tayedza Chikumbirike <t.chikumbirike@mail.utoronto.ca>
2023-03-11 21:48:54 -0500

    data cleaned and graphs created

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -19,18 +19,53 @@ raw_data <- read_dta("~/Documents/GSS_education/inputs/data/GSS2021.dta")
 
 clean_data <- clean_names(raw_data) |>
   select(
-    id,
+    id, 
     educ,
-    paeduc,
-    maeduc
+    maeduc,
+    paeduc
   ) |>
   drop_na(
     id,
     educ,
-    paeduc,
-    maeduc
+    maeduc,
+    paeduc
+  )
+
+clean_data1 <- clean_data |> 
+  mutate(
+    education = case_when(
+      educ < 8 ~ "< High School",
+      educ < 12 ~ "High School",
+      educ < 15 ~ "Some College",
+      educ < 16 ~ "College",
+      TRUE ~ "Advanced Degree"
+    )
+  )
+
+clean_data2 <- clean_data |> 
+  mutate(
+    education = case_when(
+      maeduc < 8 ~ "< High School",
+      maeduc < 12 ~ "High School",
+      maeduc < 15 ~ "Some College",
+      maeduc < 16 ~ "College",
+      TRUE ~ "Advanced Degree"
+    )
+  )
+
+clean_data3 <- clean_data |> 
+  mutate(
+    education = case_when(
+      paeduc < 8 ~ "< High School",
+      paeduc < 12 ~ "High School",
+      paeduc < 15 ~ "Some College",
+      paeduc < 16 ~ "College",
+      TRUE ~ "Advanced Degree"
+    )
   )
 
 #### Save data ####
 write_csv(clean_data, "~/Documents/GSS_education/inputs/data/clean_data.csv")
-
+write_csv(clean_data1, "~/Documents/GSS_education/inputs/data/clean_data1.csv")
+write_csv(clean_data2, "~/Documents/GSS_education/inputs/data/clean_data2.csv")
+write_csv(clean_data3, "~/Documents/GSS_education/inputs/data/clean_data3.csv")
