2927f40c7bd47036ea4f32aa7f61c7041f76ae25
Tayedza Chikumbirike <t.chikumbirike@mail.utoronto.ca>
2023-03-16 12:20:43 -0400

    Added download_data script

---
+++ scripts/01-download_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Downloads data for GSS
+# Author: Jueun Kang and Tayedza Chikumbirike
+# Data: 4 March 2023
+# Contact: jueun.kang@mail.utoronto.ca, t.chikumbirike@mail.utoronto.ca
+# Pre-requisites: N/A
+# Any other information needed? N/A
+
+library(tidyverse)
+library(janitor)
+library(dplyr)
+library(tidyr)
+library(haven)
+
+#### Download data ####
+raw_data <- read_dta("~/Documents/GSS_education/inputs/data/GSS2021.dta")
+
+#### Save raw data ####
+write_csv(raw_data, "~/Documents/GSS_education/inputs/data/raw_data.csv")
